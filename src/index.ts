283ba885678ac411856b9cdfdc64b6124475d29d
srsergio mac <sergiolazaromondargo@gmail.com>
2026-01-05 10:20:28 -0500

    publish ar component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from "./react/types.js";
+export * from "./react/TaptappAR.js";
+export * from "./react/use-ar.js";
 export * from "./compiler/offline-compiler.js";
 export { Controller } from "./compiler/controller.js";
 export { SimpleAR } from "./compiler/simple-ar.js";
