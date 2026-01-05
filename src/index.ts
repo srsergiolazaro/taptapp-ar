ca8bd316b510099d7ab8deee853542fd2f58a414
srsergio mac <sergiolazaromondargo@gmail.com>
2026-01-05 00:15:26 -0500

    fix(ci): add npm install step and update tsconfig/imports for robust build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from "./react/types";
+export * from "./react/types.js";
 export * from "./compiler/offline-compiler.js";
 export { Controller } from "./compiler/controller.js";
 export { SimpleAR } from "./compiler/simple-ar.js";
